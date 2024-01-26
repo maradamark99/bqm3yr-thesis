c4b0ab1a1f127b322ca156d890c473c1aa1c3fbb
maradamark99 <maradamark99@gmail.com>
2024-01-26 20:48:10 +0100

    fix: add getters and setters

--- backend/src/main/java/com/maradamark99/thesis/storage/Buckets.java
+++ backend/src/main/java/com/maradamark99/thesis/storage/Buckets.java
@@ -1,17 +1,15 @@
 package com.maradamark99.thesis.storage;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 @Configuration
 @ConfigurationProperties(prefix = "object-store.buckets")
-@Getter @Setter
+@Getter
+@Setter
 public class Buckets {
 
     private String product;
