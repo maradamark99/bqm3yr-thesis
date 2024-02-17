a556f70b733209fc171be6558b105253025ebaad
maradamark99 <maradamark99@gmail.com>
2024-02-17 20:35:12 +0100

    feat: create passwordEncoder bean

--- backend/src/main/java/com/maradamark99/thesis/SecurityConfig.java
+++ backend/src/main/java/com/maradamark99/thesis/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.maradamark99.thesis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -22,6 +24,11 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfigurationSource source = request -> new CorsConfiguration().applyPermitDefaultValues();
         return source;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
